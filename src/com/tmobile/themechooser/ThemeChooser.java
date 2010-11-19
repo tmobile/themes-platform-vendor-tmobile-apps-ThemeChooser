1328d75dc56765398b9ffc9e31d37cab09dad69f
Josh Guilfoyle <Josh.Guilfoyle@T-Mobile.com>
2010-11-18 17:19:32 -0800

    Improved visual appeal of reference theme chooser app.
    Switched to using a gallery view showing the theme previews, though not
    optimized as a regular application would be to handle the very large
    preview images.

--- src/com/tmobile/themechooser/ThemeChooser.java
+++ src/com/tmobile/themechooser/ThemeChooser.java
@@ -5,7 +5,6 @@ import com.tmobile.themes.provider.Themes;
 import com.tmobile.themes.widget.ThemeAdapter;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
@@ -13,29 +12,64 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ThemeChooser extends ListActivity {
+public class ThemeChooser extends Activity {
     private static final String TAG = ThemeChooser.class.getSimpleName();
+
+    private Gallery mGallery;
+    private TextView mThemeNameView;
+
+    private ThemeChooserAdapter mAdapter;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        setContentView(R.layout.main);
 
-        ThemeChooserAdapter adapter = new ThemeChooserAdapter(this);
-        adapter.setUseAutomaticMarking(true, null);
-        setListAdapter(adapter);
+        mThemeNameView = (TextView)findViewById(R.id.theme_name);
+
+        mAdapter = new ThemeChooserAdapter(this);
+        mAdapter.setUseAutomaticMarking(true, null);
+
+        mGallery = (Gallery)findViewById(R.id.gallery);
+        mGallery.setAdapter(mAdapter);
+        mGallery.setOnItemSelectedListener(mItemSelected);
+
+        Button button = (Button)findViewById(R.id.apply);
+        button.setOnClickListener(mApplyClicked);
     }
 
-    @Override
-    protected void onListItemClick(ListView list, View v, int position, long id) {
-        ThemeItem item = (ThemeItem)list.getItemAtPosition(position);
-        Uri uri = item.getUri(this);
-        Log.i(TAG, "Sending request to change to '" + item.getName() + "' (" + uri + ")");
-        Themes.changeTheme(this, uri);
-    }
+    private final OnItemSelectedListener mItemSelected = new OnItemSelectedListener() {
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            ThemeItem item = (ThemeItem)parent.getItemAtPosition(position);
+            String text = item.getName();
+            if (mAdapter.getMarkedPosition() == position) {
+                text += " (current)";
+            }
+            mThemeNameView.setText(text);
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
+    };
+
+    private final OnClickListener mApplyClicked = new OnClickListener() {
+        public void onClick(View v) {
+            int selectedPos = mGallery.getSelectedItemPosition();
+            ThemeItem item = (ThemeItem)mGallery.getItemAtPosition(selectedPos);
+            Uri uri = item.getUri(ThemeChooser.this);
+            Log.i(TAG, "Sending request to change to '" + item.getName() + "' (" + uri + ")");
+            Themes.changeTheme(ThemeChooser.this, uri);
+        }
+    };
 
     private static class ThemeChooserAdapter extends ThemeAdapter {
         public ThemeChooserAdapter(Activity context) {
@@ -44,18 +78,14 @@ public class ThemeChooser extends ListActivity {
 
         @Override
         public View newView(Context context, Cursor cursor, ViewGroup parent) {
-            return LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false);
+            return LayoutInflater.from(context).inflate(R.layout.theme_item, parent, false);
         }
 
         @Override
         public void bindView(View view, Context context, Cursor cursor) {
             ThemeItem themeItem = mDAOItem;
-            TextView vv = (TextView)view;
-            String text = themeItem.getName();
-            if (getMarkedPosition() == cursor.getPosition()) {
-                text += " (current)";
-            }
-            vv.setText(text);
+            ImageView vv = (ImageView)view;
+            vv.setImageURI(themeItem.getPreviewUri());
         }
 
         @Override
